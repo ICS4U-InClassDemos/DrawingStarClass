5af7ad357fb41b8403d617ad0b9da3a36389dab0
Tony Theodoropoulos <theodot@outlook.com>
2025-02-27 14:10:43 -0500

    completed demo

--- DrawingStarMethod/Form1.cs
+++ DrawingStarMethod/Form1.cs
@@ -12,35 +12,61 @@ namespace DrawingStarMethod
 {
     public partial class Form1 : Form
     {
+        Random randGen = new Random();
+
+        Pen blackPen = new Pen(Color.Black);
+        
+        List<Star> stars = new List<Star>();
+
         public Form1()
         {
             InitializeComponent();
+            for (int i = 0; i < 50; i++)
+            {
+                CreateStar();
+            }
+            
+        }
+
+        public void CreateStar()
+        {
+            int x = randGen.Next(0, this.Width);
+            int y = randGen.Next(0, this.Height);
+            int size = randGen.Next(10, 50);
+
+            int r = randGen.Next(0, 256);
+            int g = randGen.Next(0, 256);
+            int b = randGen.Next(0, 256);
+
+            Pen starPen = new Pen(Color.FromArgb(r, g, b));
+
+            Star newStar = new Star(starPen, x, y, size);
+            stars.Add(newStar);
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            Pen blackPen = new Pen(Color.Black);
+            foreach (Star s in stars)
+            {
+                e.Graphics.DrawPolygon(s.starPen, s.starPoints);
+            }
 
-            int x = 100;
-            int y = 50;
 
-            float size = Convert.ToSingle(200);
-            float scale = size / 206;
+        }
 
-            PointF[] starPoints = new PointF[10];
-            starPoints[0] = new PointF(0 * scale + x, 74 * scale + y);
-            starPoints[1] = new PointF(79 * scale + x, 74 * scale + y);
-            starPoints[2] = new PointF(103 * scale + x, 0 * scale + y);
-            starPoints[3] = new PointF(127 * scale + x, 74 * scale + y);
-            starPoints[4] = new PointF(206 * scale + x, 74 * scale + y);
-            starPoints[5] = new PointF(142 * scale + x, 122 * scale + y);
-            starPoints[6] = new PointF(166 * scale + x, 196 * scale + y);
-            starPoints[7] = new PointF(103 * scale + x, 150 * scale + y);
-            starPoints[8] = new PointF(39 * scale + x, 196 * scale + y);
-            starPoints[9] = new PointF(64 * scale + x, 122 * scale + y);
+        private void starTimer_Tick(object sender, EventArgs e)
+        {
+            foreach (Star s in stars)
+            {
+                s.Move();
 
-            e.Graphics.DrawPolygon(blackPen, starPoints);
-            e.Graphics.DrawRectangle(blackPen, x, y, size, size); // to test position and size
+                if (s.x > this.Width)
+                {
+                    s.x = 0 - (int)s.size;
+                }
+            }
+
+            Refresh();
         }
     }
 }
